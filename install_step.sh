91a4b69279b75cd0246495ac918800f6185fba13
Nikhil John <nikhiljohn1010@gmail.com>
2021-06-16 00:42:28 +0530

    Update install_step.sh

--- install_step.sh
+++ install_step.sh
@@ -4,7 +4,8 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 
 CLI_VER=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
-CA_VER=$(curl -s https://api.github.com/repos/smallstep/certificates/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
-wget https://github.com/smallstep/certificates/releases/download/v${CA_VER}/step-ca_${CA_VER}_amd64.deb https://github.com/smallstep/cli/releases/download/v${CLI_VER}/step-cli_${CLI_VER}_amd64.deb
+wget -o step-cli_${CLI_VER}_amd64.deb https://github.com/smallstep/cli/releases/download/v${CLI_VER}/step-cli_${CLI_VER}_amd64.deb
 dpkg -i step-cli_${CLI_VER}_amd64.deb
+CA_VER=$(curl -s https://api.github.com/repos/smallstep/certificates/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
+wget -o step-ca_${CA_VER}_amd64.deb https://github.com/smallstep/certificates/releases/download/v${CA_VER}/step-ca_${CA_VER}_amd64.deb
 dpkg -i step-ca_${CA_VER}_amd64.deb
