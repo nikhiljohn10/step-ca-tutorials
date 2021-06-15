20a23c33bbbeced5c1e780bac4835d4546531eaa
Nikhil John <nikhiljohn1010@gmail.com>
2021-06-16 00:45:20 +0530

    added cleanup script

--- install_step.sh
+++ install_step.sh
@@ -6,6 +6,8 @@ fi
 CLI_VER=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
 wget -o step-cli_${CLI_VER}_amd64.deb https://github.com/smallstep/cli/releases/download/v${CLI_VER}/step-cli_${CLI_VER}_amd64.deb
 dpkg -i step-cli_${CLI_VER}_amd64.deb
+rm step-cli_${CLI_VER}_amd64.deb
 CA_VER=$(curl -s https://api.github.com/repos/smallstep/certificates/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
 wget -o step-ca_${CA_VER}_amd64.deb https://github.com/smallstep/certificates/releases/download/v${CA_VER}/step-ca_${CA_VER}_amd64.deb
 dpkg -i step-ca_${CA_VER}_amd64.deb
+rm step-ca_${CA_VER}_amd64.deb
