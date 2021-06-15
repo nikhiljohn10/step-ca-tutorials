f7006ec206b19899f1008d25a8ab6947f694b11c
Nikhil John <nikhiljohn1010@gmail.com>
2021-06-16 00:27:59 +0530

    Install script fix

--- install_step.sh
+++ install_step.sh
@@ -1,5 +1,5 @@
 CLI_VER=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
 CA_VER=$(curl -s https://api.github.com/repos/smallstep/certificates/releases/latest | grep tag_name | sed 's/[(tag_name)"v:,[:space:]]//g')
-wget https://github.com/smallstep/certificates/releases/download/v${CA_VER}/step-ca_${CA_VER}_amd64.deb https://github.com/smallstep/cli/releases/download/v${CLI_VER}/step-ca_${CA_VER}_amd64.deb
+wget https://github.com/smallstep/certificates/releases/download/v${CA_VER}/step-ca_${CA_VER}_amd64.deb https://github.com/smallstep/cli/releases/download/v${CLI_VER}/step-ca_${CLI_VER}_amd64.deb
 sudo dpkg -i step-cli_${CLI_VER}_amd64.deb
 sudo dpkg -i step-ca_${CA_VER}_amd64.deb
