fc176f508f7f143009a94fdef1963c4ac436574e
Nikhil John <nikhiljohn1010@gmail.com>
2021-08-10 01:34:46 +0530

    Updated python server

--- scripts/server.py
+++ scripts/server.py
@@ -5,8 +5,34 @@ import argparse
 from socketserver import ThreadingMixIn
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-parser = argparse.ArgumentParser(description='Python HTTPS Server')
-parser.add_argument('-m', '--mtls', dest='mtls', action='store_true')
+parser = argparse.ArgumentParser(prog='server', description='Python HTTPS Server')
+parser.add_argument(
+    '-m', '--mtls',
+    dest='mtls',
+    action='store_true',
+    help='Enable mTLS server requirement'
+)
+parser.add_argument(
+    '-r', '--cacert',
+    dest='cacert',
+    type=str,
+    help='Provide custom CA Root Certificate',
+    default='/home/ubuntu/.step/certs/root_ca.crt',
+)
+parser.add_argument(
+    '-c', '--cert',
+    dest='cert',
+    type=str,
+    help='Provide your domain certificate',
+    default='/etc/letsencrypt/live/stepsub.multipass/privkey.pem',
+)
+parser.add_argument(
+    '-k', '--key',
+    dest='key',
+    type=str,
+    help='Provide your domain certificate\'s private key',
+    default='/etc/letsencrypt/live/stepsub.multipass/fullchain.pem',
+)
 args = parser.parse_args()
 MTLS: bool = args.mtls
 
@@ -16,7 +42,7 @@ class SimpleServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        self.wfile.write(b'\n\xf0\x9f\x91\x8b Hello, world!\n')
+        self.wfile.write(b'\xf0\x9f\x91\x8b Hello, world!\n')
 
 
 class ThreadingSimpleServer(ThreadingMixIn,HTTPServer):
@@ -29,9 +55,9 @@ if __name__ == '__main__':
     server.socket = ssl.wrap_socket(
         sock=server.socket, 
         cert_reqs=MTLS and ssl.CERT_REQUIRED or ssl.CERT_OPTIONAL,
-        ca_certs="/home/ubuntu/.step/certs/root_ca.crt",
-        keyfile="/etc/letsencrypt/live/stepsub.multipass/privkey.pem", 
-        certfile="/etc/letsencrypt/live/stepsub.multipass/fullchain.pem",
+        ca_certs=args.cacert,
+        keyfile=args.cert, 
+        certfile=args.key,
         server_side=True,
     )
     print("Server started https://stepsub.multipass")
