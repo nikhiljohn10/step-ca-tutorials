5460ea2a9021c22239c1f749152e9e550b0a4e9a
Nikhil John <nikhiljohn1010@gmail.com>
2021-08-18 01:18:05 +0530

    Upgraded python server code

--- scripts/server.py
+++ scripts/server.py
@@ -5,39 +5,6 @@ import argparse
 from socketserver import ThreadingMixIn
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-parser = argparse.ArgumentParser(prog="server", description="Python HTTPS Server")
-parser.add_argument(
-    "-m",
-    "--mtls",
-    dest="mtls",
-    action="store_true",
-    help="Enable mTLS server requirement",
-)
-parser.add_argument("-d", "--domain", dest="domain", type=str, help="Domain name")
-parser.add_argument(
-    "-r",
-    "--cacert",
-    dest="cacert",
-    type=str,
-    help="Provide custom CA Root Certificate",
-)
-parser.add_argument(
-    "-c",
-    "--cert",
-    dest="cert",
-    type=str,
-    help="Provide your domain certificate",
-)
-parser.add_argument(
-    "-k",
-    "--key",
-    dest="key",
-    type=str,
-    help="Provide your domain certificate's private key",
-)
-args = parser.parse_args()
-MTLS: bool = args.mtls
-
 
 class SimpleServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -52,21 +19,61 @@ class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(prog="server", description="Python HTTPS Server")
+    parser.add_argument(
+        "-m",
+        "--mtls",
+        dest="mtls",
+        action="store_true",
+        help="Enable mTLS server requirement",
+    )
+    parser.add_argument(
+        "-d",
+        "--domain",
+        dest="domain",
+        type=str,
+        help="Domain name",
+    )
+    parser.add_argument(
+        "-r",
+        "--cacert",
+        dest="cacert",
+        type=str,
+        help="Provide custom CA Root Certificate",
+    )
+    parser.add_argument(
+        "-c",
+        "--cert",
+        dest="cert",
+        type=str,
+        help="Provide your domain certificate",
+    )
+    parser.add_argument(
+        "-k",
+        "--key",
+        dest="key",
+        type=str,
+        help="Provide your domain certificate's private key",
+    )
+    args = parser.parse_args()
     server = ThreadingSimpleServer(("", 443), SimpleServer)
 
-    server.socket = ssl.wrap_socket(
-        sock=server.socket,
-        cert_reqs=MTLS and ssl.CERT_REQUIRED or ssl.CERT_OPTIONAL,
-        ca_certs=args.cacert,
-        certfile=args.cert,
-        keyfile=args.key,
-        server_side=True,
-    )
-    print(f"Server started https://{args.domain}")
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        pass
+    MTLS: bool = args.mtls
+    MTLS_ACTIVE_STRING: str = MTLS and "with" or "without"
+    DOMAIN: str = args.domain or server.socket.gethostname()
 
-    server.server_close()
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_verify_locations(cafile=args.cacert)
+    context.load_cert_chain(certfile=args.cert, keyfile=args.key)
+    context.verify_mode = MTLS and ssl.CERT_REQUIRED or ssl.CERT_OPTIONAL
+
+    with context.wrap_socket(sock=server.socket, server_side=True) as sock:
+        try:
+            server.socket = sock
+            print(f"Server started https://{DOMAIN} {MTLS_ACTIVE_STRING} mTLS")
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
     print("\rServer exited")
